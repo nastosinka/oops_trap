5a8539e3923e43c9d64fd886b166c5997de1dcbd
fur-ruf <161763705+fur-ruf@users.noreply.github.com>
2025-09-26 01:42:18 +0300

    Add files via upload
    add configs

---
+++ oops_trap/config/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+export default defineConfig({
+  base: './',
+  plugins: [vue()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 5173,
+    host: true,
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, '../src') 
+    }
+  }
+})
