7f72f505a5c762cecd072e174d179a671befbdcb
aneroia <shokinaa@list.ru>
2025-11-11 00:56:20 +0300

    feat: added get-endpoint for map info

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const statsRouter = require('./routes/stats');
+const mapInfoRouter = require('./routes/map-info');
 const app = express();
 
 app.use(express.json());
 app.use('/api/stats', statsRouter);
+app.use('/api/maps', mapInfoRouter);
 
 app.get('/', (req, res) => {
   res.send('<h1>Server is running!</h1><p>Ping-pong WS доступен на /ws/game</p>');
