b2cf597edf35d76f0c082743d60e34ce887be6ac
nastosinka <osinkinanastasia@gmail.com>
2025-11-12 05:21:46 +0300

    test

--- oops_trap/src/main.js
+++ oops_trap/src/main.js
@@ -6,7 +6,6 @@ import router from "./router";
 import "@mdi/font/css/materialdesignicons.css";
 
 import * as Sentry from "@sentry/vue";
-import { BrowserTracing } from "@sentry/tracing";
 
 vkBridge.send("VKWebAppInit");
 
@@ -15,13 +14,18 @@ const app = createApp(App);
 Sentry.init({
   app,
   dsn: "https://eadcbc33a1eade14aa6ae86fb5314d03@o4510349567393797.ingest.de.sentry.io/4510349569425488",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
   integrations: [
-    new BrowserTracing({
-      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-      tracingOrigins: ["localhost", /^\//],
-    }),
+    Sentry.browserTracingIntegration({ router })
   ],
-  tracesSampleRate: 1.0,
+  // Tracing
+  tracesSampleRate: 1.0, // Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^\//],
+  // Logs
+  enableLogs: true
 });
 
 app.use(createPinia());
