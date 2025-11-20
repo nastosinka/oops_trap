7743a5e5275d9f2f810508b3ef2396944b37446f
nastosinka <osinkinanastasia@gmail.com>
2025-11-21 01:02:10 +0300

    fix: linted

--- oops_trap/src/stores/user.js
+++ oops_trap/src/stores/user.js
@@ -110,8 +110,10 @@ export const useUserStore = defineStore("user", () => {
         closeGameSocket();
 
         // Создаем новое WebSocket соединение
-        const socket = new WebSocket(`ws://${import.meta.env.VITE_SERVER_IP2}/ws/game/${gameId}`);
-        
+        const socket = new WebSocket(
+          `ws://${import.meta.env.VITE_SERVER_IP2}/ws/game/${gameId}`
+        );
+
         socket.onopen = () => {
           console.log(
             "✅ Game WebSocket connected successfully for game:",
