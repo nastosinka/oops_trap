d28a854e863e1fe43e408e7a079ff0093cb847c8
aneroia <95882423+aneroia@users.noreply.github.com>
2025-12-06 19:21:45 +0300

    Added requireAuth to endpoints (#160)
    * fix: added require auth
    
    * backend: added require auth
    
    * fix: fronted added require auth and backend fix stats
    
    * backend: change endpoint for change stats to func
    
    * fix: added time in map endpoints
    
    * fix: lint format
    
    * fix: little fix in LobbyPage
    
    * fix: linted
    
    ---------
    
    Co-authored-by: AnfiNa <anfilatova.75@yandex.ru>
    Co-authored-by: nastosinka <osinkinanastasia@gmail.com>

--- backend/src/routes/stats.js
+++ backend/src/routes/stats.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const prisma = require('../db/prismaClient');
+const { requireAuth } = require('../middleware/auth');
 
 const router = express.Router();
 
@@ -9,14 +10,11 @@ const router = express.Router();
  * ===========================
  * Получить статистику игрока по всем картам
  */
-router.get('/:id_user', async (req, res) => {
-  const { id_user } = req.params;
+router.get('/:id_user', requireAuth, async (req, res) => {
+  //const { id_user } = req.params;
+  const id_user = req.user.id;
 
   try {
-    // TODO: добавить cookieAuth позже (пример)
-    // const user = await verifyUser(req.cookies);
-    // if (!user) return res.status(401).json({ error: 'Unauthorized' });
-
     const stats = await prisma.stats.findMany({
       where: { id_user: parseInt(id_user) },
       select: {
@@ -26,12 +24,6 @@ router.get('/:id_user', async (req, res) => {
       },
     });
 
-    if (!stats || stats.length === 0) {
-      return res.status(404).json({
-        error: 'Статистика для данного пользователя не найдена',
-        code: 404,
-      });
-    }
 
     const formattedUserStats = stats.map(stat => ({
       map_id: stat.id_map,
@@ -49,114 +41,4 @@ router.get('/:id_user', async (req, res) => {
     });
   }
 });
-
-/**
- * ===========================
- * POST /api/stats
- * ===========================
- * Создать или обновить статистику
- * Обновляет существующую запись, если найдена с тем же id_user, id_map и role - переписать в функцию????
- */
-router.post('/', async (req, res) => {
-  try {
-    const { id_user, id_map, time, role } = req.body;
-
-    if (id_user === undefined || id_map === undefined || time === undefined || role === undefined) {
-      return res.status(400).json({ 
-        error: 'Обязательные поля: id_user, id_map, time, role',
-        code: 400,
-      });
-    }
-
-    const userId = parseInt(id_user);
-    const mapId = parseInt(id_map);
-    const timeValue = parseInt(time);
-
-    if (isNaN(userId) || isNaN(mapId) || isNaN(timeValue)) {
-      return res.status(400).json({ 
-        error: 'Поля id_user, id_map и time должны быть числами',
-        code: 400,
-      });
-    }
-
-    if (!(typeof role === 'boolean')) {
-        return res.status(400).json({ 
-        error: 'Поле role должно быть булевым значением',
-        code: 400,
-      });
-    }
-
-    const existingStat = await prisma.stats.findFirst({
-      where: {
-        id_user: parseInt(id_user),
-        id_map: parseInt(id_map),
-        role: role,
-      },
-    });
-
-    let result;
-    let action;
-
-    if (existingStat) {
-      if (existingStat.time > parseInt(time)){
-      result = await prisma.stats.update({
-        where: {
-          id: existingStat.id,
-        },
-        data: {
-          time: parseInt(time),
-        },
-      });
-      action = 'updated';
-      console.log('Статистика обновлена:', result);
-    }else{
-      console.log('Статистика не требует обновлений');
-      result = existingStat;
-    }
-    } else {
-      result = await prisma.stats.create({
-        data: {
-          id_user: parseInt(id_user),
-          id_map: parseInt(id_map),
-          time: parseInt(time),
-          role: role,
-        },
-      });
-      action = 'created';
-      console.log('Новая статистика создана:', result);
-    }
-
-    const formattedResult = {
-      id: result.id,
-      id_user: result.id_user,
-      id_map: result.id_map,
-      time: result.time,
-      role: result.role,
-    };
-
-    res.status(200).json({
-      success: true,
-      action: action,
-      data: formattedResult,
-    });
-
-  } catch (error) {
-    console.error('Ошибка при сохранении статистики:', error);
-    
-    if (error.code === 'P2003') {
-      return res.status(400).json({
-        error: 'Неверный id_user или id_map',
-        code: 400,
-        details: 'Указанный пользователь или карта не существует'
-      });
-    }
-
-    res.status(500).json({
-      error: 'Ошибка сервера при сохранении статистики',
-      code: 500,
-      details: error.message
-    });
-  }
-});
-
 module.exports = router;
